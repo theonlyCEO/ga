d68534c0dfead014606a16115cd8e36dfd21f2be
wilfred <wilfredmvula@icloud.com>
2025-10-17 14:43:19 +0200

    updated

--- index.js
+++ index.js
@@ -11,19 +11,16 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://gadget-store-lilac.vercel.app/"
+    "https://gadget-store-lilac.vercel.app/",
+"https://gadget-store-git-master-theonlyceos-projects.vercel.app/",
+"https://gadget-store-q6pkb44wx-theonlyceos-projects.vercel.app/"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 let client, db;
 
