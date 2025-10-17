416264bb3f2c2b7c263b9775076e889d567896ad
wilfred <wilfredmvula@icloud.com>
2025-10-17 12:34:11 +0200

    domain

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    "https://vercel.com/theonlyceos-projects/gadget-store"
+    "gadget-store-lilac.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
